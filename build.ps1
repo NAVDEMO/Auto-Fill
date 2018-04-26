6d947e5bb656d205445bd181c0314048a86e6ed5
freddydk <freddy.kristiansen@microsoft.com>
2018-04-26 20:01:00 +0200

    Initial commit

---
+++ build.ps1
@@ -0,0 +1,34 @@
+Write-Host -ForegroundColor Green "Installing NavContainerHelper"
+$localrepo = "${env:USERPROFILE}\Documents\GitHub\microsoft\navcontainerhelper\NavContainerHelper.ps1"
+if (Test-Path -Path $localrepo) {
+    . $localrepo
+} else {
+    Install-Module NavContainerHelper -force
+}
+
+$appProjectFolder = $PSScriptRoot
+$agentFolder = $appProjectFolder.Substring(0,$appProjectFolder.IndexOf('\',3))
+
+$imageName = "microsoft/bcsandbox"
+$containerName = "compiler"
+$credential = [PSCredential]::new("admin", (ConvertTo-SecureString -String "P@ssword1" -AsPlainText -Force))
+New-NavContainer -accept_eula `
+                 -accept_outdated `
+                 -alwaysPull `
+                 -containerName $containerName `
+                 -imageName $imageName `
+                 -auth NAVUserPassword `
+                 -Credential $credential `
+                 -updateHosts `
+                 -shortcuts None `
+                 -additionalParameters @("--volume ""${agentFolder}:c:\source""", "--env httpSite=N", "--env WebClient=N") `
+                 -myScripts @(@{'MainLoop.ps1' = 'while ($true) { start-sleep -seconds 10 }'})
+
+Write-Host -ForegroundColor Green "Build started"
+Compile-AppInNavContainer -containerName $containerName -credential $credential -appProjectFolder $appProjectFolder
+
+Write-Host "Remove Container"
+#Remove-NavContainer -containerName $containerName
+
+Write-Host "Remove unused images"
+#docker system prune -f
